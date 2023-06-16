854abfc79c4f355e99e25c2a898b09366b3d62e5
dafnemus <dafnemus@gmail.com>
2023-06-16 09:48:04 -0300

    Complete excersice 1 of loops in js

---
+++ ejercicios-J2/E1/index.js
@@ -0,0 +1,6 @@
+// Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc.
+// (No se ingresan valores por teclado).
+
+for (let i = 0; i < 25; i++) {
+  console.log(i, 11 * i);
+}
