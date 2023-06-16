429043e0b2a5b9a6a7b04a3670bd2dce16cbbe4e
dafnemus <dafnemus@gmail.com>
2023-06-16 09:58:43 -0300

    Complete excersice 9 of loops in js

---
+++ ejercicios-J2/E9/index.js
@@ -0,0 +1,28 @@
+/*
+Confeccionar un programa que lea 3 pares de datos, cada par de datos corresponde a la medida de la base y la altura de un triángulo. El programa deberá informar:
+  De cada triángulo la medida de su base, su altura y su superficie.
+  La cantidad de triángulos cuya superficie es mayor a 12.
+*/
+
+const triangles = [
+    {
+      base: 15,
+      height: 8,
+    },
+    {
+      base: 9,
+      height: 14,
+    },
+    {
+      base: 10,
+      height: 5,
+    },
+  ];
+  
+  triangles.map(({ base, height }) => {
+    let superficie = (base * height) / 2;
+    console.log(
+      `Triángulo con base: ${base}cm y altura: ${height}cm posee una superficie de ${superficie}cm`
+    );
+  });
+  
