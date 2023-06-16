087e9038816519d6bcd60667a7fa11fdec26c871
dafnemus <dafnemus@gmail.com>
2023-06-16 10:01:02 -0300

    Complete excersice 11 of loops in js

---
+++ ejercicios-J2/E11/index.js
@@ -0,0 +1,9 @@
+// Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50).
+let multiplicationTable5 = [];
+
+for (let i = 1; i <= 10; i++) {
+  let value = i * 5;
+  multiplicationTable5.push(value);
+}
+
+console.log(multiplicationTable5);
