6b6f3854709ebcf32caaa6638ae211147ebe370b
dafnemus <dafnemus@gmail.com>
2023-06-16 10:35:57 -0300

    Complete excersice 1 of basic js

---
+++ ejercicios-JS1/E1/index.js
@@ -0,0 +1,10 @@
+/*
+Haz un programa en JS que define e inicializa una variable de tipo cadena de caracteres donde asignaremos el nombre de un empleado, 
+otra variable de tipo entera donde se asignaremos su sueldo y una constante de tipo numérica donde asignaremos la edad.
+*/
+
+let nameEmpleoyee = "Pablo";
+let salary = 1000;
+const AGE = 25;
+
+console.log(nameEmpleoyee, salary, AGE);
